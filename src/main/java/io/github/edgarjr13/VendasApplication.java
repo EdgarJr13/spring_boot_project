2d3625e2c38dfdb5d775c4b43b70b9d75287fdfa
Edgar Camara <ecamara788@beijaflore.com>
2022-12-19 14:17:30 -0300

    salvando e recuperando objetos do banco.

--- src/main/java/io/github/edgarjr13/VendasApplication.java
+++ src/main/java/io/github/edgarjr13/VendasApplication.java
@@ -1,10 +1,29 @@
 package io.github.edgarjr13;
 
+import io.github.edgarjr13.domain.entity.Cliente;
+import io.github.edgarjr13.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication{
+
+
+    @Bean
+    public CommandLineRunner init (@Autowired Clientes clientes){
+        return args -> {
+            clientes.salvar(new Cliente("Edgar"));
+            clientes.salvar(new Cliente("Stephane"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
