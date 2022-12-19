84fe0b5841d3e36e37f52fbf226c4c977bc38c48
Edgar Camara <ecamara788@beijaflore.com>
2022-12-19 11:58:13 -0300

    criando modelos do schema;

--- src/main/java/io/github/edgarjr13/VendasApplication.java
+++ src/main/java/io/github/edgarjr13/VendasApplication.java
@@ -1,40 +1,10 @@
 package io.github.edgarjr13;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@ComponentScan(
-//        basePackages = {
-//            "io.github.edgarj13.repository",
-//            "io.github.edgarjr13.service"
-//        }
-//)
-@RestController
-public class VendasApplication {
-
-    //utilizanto interpolação para chamar Strings do application.properties
-    @Value("${application.name}")
-    private String applicationName;
-
-    private Animal animal;
-
-    @Bean(name = "executarAnimal")
-    public CommandLineRunner executar() {
-        return args -> {
-            this.animal.fazerBarulho();
-        };
-    }
-
-    @GetMapping("/hello")
-    public String helloWorld(){
-        return applicationName;
-    }
+public class VendasApplication{
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
